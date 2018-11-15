e0f45fe3e641eb7678c9a6cd1b2aa46fde24d73a
leonardoarruda <leonardoarruda1984@gmail.com>
2018-11-15 00:23:54 -0200

    implementando demais cruds

--- uploads/core/views.py
+++ uploads/core/views.py
@@ -7,20 +7,18 @@ from uploads.core.forms import DocumentForm
 from google.cloud import storage
 
 def home(request):
-    return render(request, 'core/home.html')
+    """List files"""
+    blobs = list_blobs('upload_files_leonardo')
+    return render(request, 'core/home.html', {
+        'blobs': blobs
+    })
 
-def upload_to_bucket(blob_name, path_to_file, bucket_name):
-    """Upload data to a bucket"""
-    storage_client = storage.Client()
+def remove(request):
+    """Confirm delete file"""
+    delete_file(request.GET['filename'])
+    return render(request, 'core/remove.html')
 
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(blob_name)
-    blob.upload_from_filename(path_to_file)
-
-    #returns a public url
-    return blob.public_url
-
-def simple_upload(request):
+def add(request):
     """Download data to a server"""
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
@@ -28,8 +26,34 @@ def simple_upload(request):
         filename = fs.save(myfile.name, myfile)
         filepath = './media/{}'.format(filename)
         uploaded_file_url = upload_to_bucket(filename, filepath, 'upload_files_leonardo')
-        return render(request, 'core/simple_upload.html', {
+        return render(request, 'core/add.html', {
             'uploaded_file_url': uploaded_file_url
         })
-    return render(request, 'core/simple_upload.html')
+    return render(request, 'core/add.html')
 
+#### GOOGLE CLOUD ####
+def delete_file(filename):
+    """Removes file from bucket"""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket('upload_files_leonardo')
+    blob = bucket.blob(filename)
+
+    blob.delete()
+
+def list_blobs(bucket_name):
+    """Lists all the blobs in the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    return bucket.list_blobs()
+
+def upload_to_bucket(blob_name, path_to_file, bucket_name):
+    """Upload data to a bucket"""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(path_to_file)
+
+    #returns a public url
+    return blob.public_url
--- uploads/urls.py
+++ uploads/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,8 +8,10 @@ from uploads.core import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^uploads/simple/$', views.simple_upload, name='simple_upload'),
+    url(r'^uploads/add/$', views.add, name='add'),
+    url(r'^uploads/remove/', views.remove, name='remove'),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls')),
 ]
 
 if settings.DEBUG:
